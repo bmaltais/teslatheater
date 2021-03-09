9d0aee63d20e100d7a775536673f900583850626
Bernard Maltais <bernard.maltais@canada.ca>
2021-03-09 13:34:28 +0000

    Update code

--- teslatheater/src/App.svelte
+++ teslatheater/src/App.svelte
@@ -7,7 +7,8 @@
 	<p></p>
 	<p><a href="https://www.youtube.com/redirect?q=https://bmaltais.github.io/"><img src=logos/fullscreen.png alt="GO FULL SCREEN" style="height:200px;"></a></p>
 	<p></p>
-	<p><a href="https://tv.bell.ca/"><img src=logos/bell.png alt="Bell Fibe TV" style="height:200px;"></a><a href="https://www.disneyplus.com/fr-ca/login"><img src=logos/disney.svg alt="Disney+" style="height:200px;"></a></p>
+	<p><a href="https://tv.bell.ca/"><img src=logos/fibetv.png alt="Bell Fibe TV" style="height:200px;"></a>
+	<a href="https://www.disneyplus.com/fr-ca/login"><img src=logos/disney.svg alt="Disney+" style="height:200px;"></a></p>
 </main>
 
 <style>
