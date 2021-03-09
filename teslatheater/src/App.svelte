492bdc37699a4b25699867bef43d82ea2d23c29d
Bernard Maltais <bernard.maltais@canada.ca>
2021-03-09 16:08:50 +0000

    Update Logos

--- teslatheater/src/App.svelte
+++ teslatheater/src/App.svelte
@@ -5,17 +5,16 @@
 <main>
 	<p><a href="https://www.youtube.com/redirect?q=https://bmaltais.github.io/"><img src=logos/fullscreen.png alt="GO FULL SCREEN"></a></p>
 	<p>
-	<a href="https://www.crave.ca/en"><img src=logos/crave.gif alt="Crave" align="middle" hspace="10" vspace="10"></a>
-	<a href="https://www.disneyplus.com/fr-ca/login"><img src=logos/disney.svg alt="Disney+" align="middle" hspace="10" vspace="10"></a>
-	<a href="https://www.crave.ca/en/hbo"><img src=logos/hbogo.gif alt="HBO" align="middle" hspace="10" vspace="10"></a>
-	<a href="https://tv.bell.ca/"><img src=logos/fibetv.png alt="Bell Fibe TV" align="middle" hspace="10" vspace="10"></a>
-	<a href="https://netflix.com/"><img src=logos/netflix.jpg alt="Tubi TV" align="middle" hspace="10" vspace="10"></a>
-	<a href="https://www.paramountplus.com/ca/"><img src=logos/paramountplus.png alt="Paramount+" align="middle" hspace="10" vspace="10"></a>
-	<a href="https://ici.tou.tv/"><img src=logos/logo-toutv.svg alt="TouTV" align="middle" hspace="10" vspace="10"></a>
-	<a href="https://tubitv.com/"><img src=logos/tubitv.gif alt="Tubi TV" align="middle" hspace="10" vspace="10"></a>
-	<a href="https://www.twitch.com"><img src=logos/twitch.gif alt="Twitch" align="middle" hspace="10" vspace="10"></a>
-	<a href="https://www.youtube.com"><img src=logos/youtube.jpg alt="Youtube" align="middle" hspace="10" vspace="10"></a>
-	<a href="https://youtube.tv"><img src=logos/youtubetv.jpg alt="Youtube TV" align="middle" hspace="10" vspace="10"></a>
+	<a href="https://www.crave.ca/en"><img src=logos/crave.png alt="Crave" align="middle" hspace="20" vspace="10"></a>
+	<a href="https://www.disneyplus.com/fr-ca/login"><img src=logos/disney.svg alt="Disney+" align="middle" hspace="20" vspace="10"></a>
+	<a href="https://www.crave.ca/en/hbo"><img src=logos/hbogo.gif alt="HBO" align="middle" hspace="20" vspace="10"></a>
+	<a href="https://tv.bell.ca/"><img src=logos/bell.png alt="Bell Fibe TV" align="middle" hspace="20" vspace="10"></a>
+	<a href="https://netflix.com/"><img src=logos/netflix.png alt="Tubi TV" align="middle" hspace="20" vspace="10"></a>
+	<a href="https://www.paramountplus.com/ca/"><img src=logos/paramountplus.png alt="Paramount+" align="middle" hspace="20" vspace="10"></a>
+	<a href="https://ici.tou.tv/"><img src=logos/toutv.png alt="TouTV" align="middle" hspace="20" vspace="10"></a>
+	<a href="https://tubitv.com/"><img src=logos/tubitv.png alt="Tubi TV" align="middle" hspace="20" vspace="10"></a>
+	<a href="https://www.twitch.com"><img src=logos/twitch.png alt="Twitch" align="middle" hspace="20" vspace="10"></a>
+	<a href="https://www.youtube.com"><img src=logos/youtube.jpg alt="Youtube" align="middle" hspace="20" vspace="10"></a>
 	</p>
 </main>
 
@@ -36,6 +35,11 @@
 
 	img {
     width: 20%;
+    height: auto;
+	}
+
+	.img-large, .img-large img {
+    width: 50%;
     height: auto;
 	}
 
