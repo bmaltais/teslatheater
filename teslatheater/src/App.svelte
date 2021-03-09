66c9ac62eba6ad58465b04f54a49a81e847f0541
Bernard Maltais <bernard.maltais@canada.ca>
2021-03-09 15:24:59 +0000

    Add more video apps

--- teslatheater/src/App.svelte
+++ teslatheater/src/App.svelte
@@ -3,12 +3,20 @@
 </script>
 
 <main>
-	<h1>Welcome to {name}!</h1>
-	<p></p>
-	<p><a href="https://www.youtube.com/redirect?q=https://bmaltais.github.io/"><img src=logos/fullscreen.png alt="GO FULL SCREEN" style="height:200px;"></a></p>
-	<p></p>
-	<p><a href="https://tv.bell.ca/"><img src=logos/fibetv.png alt="Bell Fibe TV" style="height:200px;"></a>
-	<a href="https://www.disneyplus.com/fr-ca/login"><img src=logos/disney.svg alt="Disney+" style="height:200px;"></a></p>
+	<p><a href="https://www.youtube.com/redirect?q=https://bmaltais.github.io/"><img src=logos/fullscreen.png alt="GO FULL SCREEN"></a></p>
+	<p>
+	<a href="https://www.crave.ca/en"><img src=logos/crave.gif alt="Crave" align="middle" hspace="10" vspace="10"></a>
+	<a href="https://www.disneyplus.com/fr-ca/login"><img src=logos/disney.svg alt="Disney+" align="middle" hspace="10" vspace="10"></a>
+	<a href="https://www.crave.ca/en/hbo"><img src=logos/hbogo.gif alt="HBO" align="middle" hspace="10" vspace="10"></a>
+	<a href="https://tv.bell.ca/"><img src=logos/fibetv.png alt="Bell Fibe TV" align="middle" hspace="10" vspace="10"></a>
+	<a href="https://netflix.com/"><img src=logos/netflix.jpg alt="Tubi TV" align="middle" hspace="10" vspace="10"></a>
+	<a href="https://www.paramountplus.com/ca/"><img src=logos/paramountplus.png alt="Paramount+" align="middle" hspace="10" vspace="10"></a>
+	<a href="https://ici.tou.tv/"><img src=logos/logo-toutv.svg alt="TouTV" align="middle" hspace="10" vspace="10"></a>
+	<a href="https://tubitv.com/"><img src=logos/tubitv.gif alt="Tubi TV" align="middle" hspace="10" vspace="10"></a>
+	<a href="https://www.twitch.com"><img src=logos/twitch.gif alt="Twitch" align="middle" hspace="10" vspace="10"></a>
+	<a href="https://www.youtube.com"><img src=logos/youtube.jpg alt="Youtube" align="middle" hspace="10" vspace="10"></a>
+	<a href="https://youtube.tv"><img src=logos/youtubetv.jpg alt="Youtube TV" align="middle" hspace="10" vspace="10"></a>
+	</p>
 </main>
 
 <style>
@@ -24,6 +32,11 @@
 		text-transform: uppercase;
 		font-size: 4em;
 		font-weight: 100;
+	}
+
+	img {
+    width: 20%;
+    height: auto;
 	}
 
 	@media (min-width: 640px) {
